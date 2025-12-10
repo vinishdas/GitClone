052fe7684323bb33b1db13fe137287512b7f5183
Dvinish <dvinish6669@gmail.com>
2025-12-10 12:12:48 +0530

    initial project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-semibold">GPT Clone</h1>
+      
+
+    </main>
+  );
+}
